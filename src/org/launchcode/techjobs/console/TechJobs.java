939c2da06794c17d7798a70b06158c07ec39fa52
lucible <gobbldygook@letterboxes.org>
2020-01-13 19:25:30 -0600

    completed printJobs method

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -110,7 +110,16 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("\nNo search results found!");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("*****");
+                for (String field : job.keySet()) {
+                    System.out.println(field + ": " + job.get(field));
+                }
+            };
+            System.out.println("*****");
+        }
     }
 }
